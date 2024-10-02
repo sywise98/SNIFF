65706a3a8ecf75358925345ae983801c0d12e729
swise30 <82410530+swise30@users.noreply.github.com>
2024-10-02 14:15:34 -0400

    labeling parts

--- wave_detect.py
+++ wave_detect.py
@@ -68,16 +68,21 @@ while cv.waitKey(1) < 0:
     for pair in POSE_PAIRS:
         partFrom = pair[0]
         partTo = pair[1]
+        
         assert(partFrom in BODY_PARTS)
         assert(partTo in BODY_PARTS)
 
         idFrom = BODY_PARTS[partFrom]
         idTo = BODY_PARTS[partTo]
+        
 
         if points[idFrom] and points[idTo]:
             cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
             cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
             cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            
+            cv.putText(frame, f'{pair[0]}', points[idFrom], cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+            cv.putText(frame, f'{pair[1]}', points[idTo], cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
 
     t, _ = net.getPerfProfile()
     freq = cv.getTickFrequency() / 1000
